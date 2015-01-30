bffb2a8b9abe3df8353f4fe51264841b95f47541
jreyna13 <jreyna13@mail.stmarytx.edu>
2015-01-29 23:41:47 -0600

    Initial check in for variables softdrink project.

---
+++ Challenge15/src/SoftDrink.java
@@ -0,0 +1,36 @@
+//Javier Reyna EG1305A
+
+import java.text.DecimalFormat;
+
+
+public class SoftDrink {
+	
+	public static void main(String[] args) {
+		
+		double customers = 12467;
+		
+		double purchased = .14;
+		
+		double citrus = .64;
+		
+		DecimalFormat aFormatter = new DecimalFormat("##,###.");
+		System.out.println("Total number of people surveyed "+ aFormatter.format(customers));
+		
+		double percentbuys = customers * purchased;
+		
+		DecimalFormat bFormatter = new DecimalFormat("####");
+		
+		System.out.println("Approximately " + bFormatter.format(percentbuys) + " bought at least one drink." );
+		
+		double prefercitrus = percentbuys * citrus;
+		
+		DecimalFormat cFormatter = new DecimalFormat("####");
+		
+		System.out.println(cFormatter.format (prefercitrus) + " of those who purchased a drink "
+				+ "prefer citrus flavor drinks.");
+		
+		System.exit(0);
+		
+		
+	}
+}
